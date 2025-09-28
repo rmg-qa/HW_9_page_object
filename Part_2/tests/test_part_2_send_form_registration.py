4806c84948c3534f57432015488da43e1bf54982
rmg-qa <gorohovikroman123@yandex.com>
2025-09-28 15:31:56 +0700

    Выполнение 2 части домашней работы по Page Object

---
+++ Part_2/tests/test_part_2_send_form_registration.py
@@ -0,0 +1,11 @@
+from Part_2.pages.page_registration_form import RegistrationFormMethods, SimpleRegistration
+from Part_2.data.user import user_student
+
+
+def test_send_form_part_2(browser_settings):
+    user = user_student
+    open_page_registrations = RegistrationFormMethods()
+    simple_registrations = SimpleRegistration()
+    open_page_registrations.open_page()  # открытие страницы регистрации
+    simple_registrations.simple_registration(user)  # регистрация нового пользователя
+    simple_registrations.assert_simple_registration()  # проверка, что регистрация прошла успешно
